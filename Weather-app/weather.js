7b46af018e6b6e09368010a16b6f3d229b5c8c32
doannhu <ndodoan@gmail.com>
2022-06-19 22:47:45 +1000

    Update weather.js

--- Weather-app/weather.js
+++ Weather-app/weather.js
@@ -19,12 +19,3 @@ class Weather {
     }
 }
 
-
-
-const weather = new Weather('Melbourne');
-
-weather.getWeather()
-    .then(results => {
-        console.log(results);
-    })
-    .catch(err => console.log(err));
