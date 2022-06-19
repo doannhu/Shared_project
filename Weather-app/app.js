278f0433f1403f66738ff00920be4b69b7e82d1a
doannhu <ndodoan@gmail.com>
2022-06-19 22:47:11 +1000

    Update app.js

--- Weather-app/app.js
+++ Weather-app/app.js
@@ -1,7 +1,25 @@
-// const weather = new Weather('Melbourne');
+const weather = new Weather('Melbourne');
+const ui = new UI();
 
-// weather.getWeather()
-//     .then(results => {
-//         console.log(results);
-//     })
-//     .catch(err => console.log(err));
+
+weather.getWeather()
+    .then(results => {
+        console.log(results);
+        ui.passInfo(results);
+    })
+    .catch(err => console.log(err));
+
+
+
+document.getElementById("w-change-btn").addEventListener("click",function() {
+    let weatherUpdated = new Weather(document.getElementById("city").value);
+    let uiUpdated = new UI();
+    weatherUpdated.getWeather()
+    .then(results => {
+        uiUpdated.passInfo(results);
+        console.log(results);
+    })
+    .catch(err => console.log(err));
+    document.getElementById("w-location").textContent = document.getElementById("city").value.toUpperCase();
+    
+  });
