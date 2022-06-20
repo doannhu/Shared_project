1aed8667952d47d33425c5634ff33cc6b083d85a
doannhu <ndodoan@gmail.com>
2022-06-20 22:50:56 +1000

    Create weather.js

---
+++ Weather-app(backend Node js Express)/public/weather.js
@@ -0,0 +1,21 @@
+class Weather {
+    constructor(city) {
+        this.apiKey = '5b52d51bb07248f2a8f54837221806';
+        this.city = city;
+    }
+
+    // Fetch weather from API
+    async getWeather() {
+        const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${this.apiKey}&q=${this.city}&aqi=no`);
+
+        const responseData = await response.json();
+
+        return responseData.current;
+    }
+
+    // Change weather location
+    changeLocation(city) {
+        this.city = city;
+    }
+}
+
